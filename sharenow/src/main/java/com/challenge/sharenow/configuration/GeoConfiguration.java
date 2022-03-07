112d9567377a779ceae9cf42f3bc3bccf86153f6
adel.ramezani <adramazany@gmail.com>
2022-03-07 14:03:44 +0330

    swagger finished

--- sharenow/src/main/java/com/challenge/sharenow/configuration/GeoConfiguration.java
+++ sharenow/src/main/java/com/challenge/sharenow/configuration/GeoConfiguration.java
@@ -12,14 +12,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableScheduling
+@EnableSwagger2
 public class GeoConfiguration {
 
     @Bean
     RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+//                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.challenge.sharenow"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
